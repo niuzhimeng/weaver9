dbaed6e9002c203814b2a4ec71539e1cd9d2f9ce
295290968@qq.com <295290968@qq.com>
2020-08-31 18:43:38 +0800

    国航报表

--- src/com/weavernorth/guohang/timed/CreateReportBySource.java
+++ src/com/weavernorth/guohang/timed/CreateReportBySource.java
@@ -1,5 +1,6 @@
 package com.weavernorth.guohang.timed;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import weaver.conn.RecordSet;
@@ -22,13 +23,16 @@ public class CreateReportBySource extends BaseCronJob {
         MY_LOG.info("生成报表计划任务Start========" + TimeUtil.getCurrentTimeString());
 
         try {
+            // 类型 + 部门 + 年 + 月 确定一条唯一数据
             RecordSet updateErp = new RecordSet();
-            updateErp.executeQuery("select bm, n, y from uf_tbscdj");
+            updateErp.executeQuery("select lx, bm, n, y from uf_tbscdj");
             int erpCounts = updateErp.getCounts();
             List<String> erpExistList = new ArrayList<>(erpCounts); // 生产单价实体表中已存在的数据
             while (updateErp.next()) {
-                erpExistList.add(updateErp.getString("bm") + updateErp.getString("n") + updateErp.getString("y"));
+                erpExistList.add(updateErp.getString("lx") + updateErp.getString("bm")
+                        + updateErp.getString("n") + updateErp.getString("y"));
             }
+            //MY_LOG.info("erpExistList: " + JSONObject.toJSONString(erpExistList));
 
             // 生产单价虚拟表（数据源是ERP）
             RecordSetDataSource erpSource = new RecordSetDataSource("ERP");
@@ -40,12 +44,13 @@ public class CreateReportBySource extends BaseCronJob {
                 String lx = erpSource.getString("lx"); // 类型
                 String bm = erpSource.getString("bm"); // 部门
                 String year = erpSource.getString("YEAR"); // 年
-                String month = erpSource.getString("MONTH"); // 月
-                String prod_price = erpSource.getString("PROD_PRICE"); // 生产单价
+                String month = monthDeleteZero(erpSource.getString("MONTH")); // 月
+                String prod_price = null2Double(erpSource.getString("PROD_PRICE"), "0.0000"); // 生产单价
 
-                String NO_PROD_PRICE = erpSource.getString("NO_PROD_PRICE"); // 非生产单价
+                String NO_PROD_PRICE = null2Double(erpSource.getString("NO_PROD_PRICE"), "0.0000"); // 非生产单价
 
-                if (erpExistList.contains(bm + year + month)) {
+                // MY_LOG.info("lx+bm + year + month: " + lx + bm + year + month + "; contains: " + erpExistList.contains(lx + bm + year + month));
+                if (erpExistList.contains(lx + bm + year + month)) {
                     continue;
                 }
                 updateErp.executeUpdate(insertErp, lx, bm, year, month, prod_price, NO_PROD_PRICE);
@@ -53,11 +58,12 @@ public class CreateReportBySource extends BaseCronJob {
 
             // 生产数据虚拟表（数据源是GHSCTJ）
             RecordSet updateGHSCTJ = new RecordSet();
-            updateGHSCTJ.executeQuery("select bm, n, y from uf_tbscsj");
-            int counts = updateErp.getCounts();
+            updateGHSCTJ.executeQuery("select lx, bm, n, y from uf_tbscsj");
+            int counts = updateGHSCTJ.getCounts();
             List<String> GHSCTJExistList = new ArrayList<>(counts); // 生产数据实体表中已存在的数据
             while (updateGHSCTJ.next()) {
-                GHSCTJExistList.add(updateGHSCTJ.getString("bm") + updateGHSCTJ.getString("n") + updateGHSCTJ.getString("y"));
+                GHSCTJExistList.add(updateGHSCTJ.getString("lx") + updateGHSCTJ.getString("bm") +
+                        updateGHSCTJ.getString("n") + updateGHSCTJ.getString("y"));
             }
 
             RecordSetDataSource GHSCTJSource = new RecordSetDataSource("GHSCTJ");
@@ -70,15 +76,15 @@ public class CreateReportBySource extends BaseCronJob {
                 String lx = GHSCTJSource.getString("lx"); // 类型
                 String bm = GHSCTJSource.getString("bm"); // 部门
                 String year = GHSCTJSource.getString("YEAR"); // 年
-                String month = GHSCTJSource.getString("MONTH"); // 月
-                String schbxhl = GHSCTJSource.getString("schbxhl"); // 生产航班消耗量
+                String month = monthDeleteZero(GHSCTJSource.getString("MONTH")); // 月
+                String schbxhl = null2Double(GHSCTJSource.getString("schbxhl"), "0.0000"); // 生产航班消耗量
 
-                String fschbxhl = GHSCTJSource.getString("fschbxhl"); // 非生产航班消耗量
-                String hkmyxhl = GHSCTJSource.getString("hkmyxhl"); // 航空煤油消耗量
-                String yszzl = GHSCTJSource.getString("yszzl"); // 运输周转量
-                String fxxs = GHSCTJSource.getString("fxxs"); // 飞行小时数
+                String fschbxhl = null2Double(GHSCTJSource.getString("fschbxhl"), "0.0000"); // 非生产航班消耗量
+                String hkmyxhl = null2Double(GHSCTJSource.getString("hkmyxhl"), "0.0000"); // 航空煤油消耗量
+                String yszzl = null2Double(GHSCTJSource.getString("yszzl"), "0.00"); // 运输周转量
+                String fxxs = null2Double(GHSCTJSource.getString("fxxs"), "0.00"); // 飞行小时数
 
-                if (GHSCTJExistList.contains(bm + year + month)) {
+                if (GHSCTJExistList.contains(lx + bm + year + month)) {
                     continue;
                 }
                 updateGHSCTJ.executeUpdate(insertGHSCTJ,
@@ -127,6 +133,28 @@ public class CreateReportBySource extends BaseCronJob {
             MY_LOG.info("生成报表异常： " + e);
         }
     }
+
+    /**
+     * 如果值为空，则返回代替值
+     */
+    private String null2Double(String value, String replaceValue) {
+        if (StringUtils.isBlank(value)) {
+            return replaceValue;
+        } else {
+            return value;
+        }
+    }
+
+    /**
+     * 如果月份是小于10的，删除前面的0（数据源获取的是01,02这种格式，oa表中是整数字段，获取到的是1,2）
+     */
+    private String monthDeleteZero(String month) {
+        if (month.startsWith("0")) {
+            month = month.substring(1);
+        }
+        return month;
+    }
+
 }
 
 
